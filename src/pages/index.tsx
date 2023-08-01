717834a152091c5c8c0ac939c5f5d6096d71f97e
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-01 17:16:08 -0300

    Configurando Stitches para estilos
    Para o CSS entrar no servidor Node do Next, mesmo desabilitando o JavaScript, precisamos incluir a tag com style com o id do stitches com uma propriedade que pegará o CSS. Tem na documentação do stitches em Server-side rendering.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Inter } from 'next/font/google';
+import { styled } from "@/styles";
 
-const inter = Inter({ subsets: ['latin'] });
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+  cursor: 'pointer',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+    transition: '0.5s',
+  },
+
+});
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Teste </span>
+      Enviar
+    </Button>
   );
 }
