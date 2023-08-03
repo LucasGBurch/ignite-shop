833d06227b328e7295e74b91b2227e446f73a17b
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-03 15:41:30 -0300

    Carrossel para estrutura da Home
    Efeito carrossel para exibição das imagens de produtos/camisetas em pedaços. Aplicando a biblioteca keen-slider e adicionando as configurações correspondentes. Mas foi necessário retirar os gap de HomeContainer para não bugar. Pois o keen não contabiliza esses espaçamentos. Isso também vale para o padding de Product. Nos trocamos essas partes por spacing do próprio keen-slider.

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from '..';
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   // backgroundColor: 'red', // só para visualizar o Container durante as mudanças abaixo:
 
   // Forma do container das camisetas/produtos cobrir do centro para a direita da tela, para podermos aplicar o carrossel
@@ -15,7 +14,6 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
   background: 'linear-gradient(100deg, #1EA483 0%, #7465D4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden', // Para não enxergar o footer fora no efeito hover
