ab9439653521999360cbaf0db9de706b35fb46ac
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-11 17:25:39 -0300

    Carregando dados do Produto
    Estrutura para carregar dados do produto. Para funcionar sem erro, falta uma parte vista na próxima aula, sobre Static-Site Generation (SSG) com parâmetros dinâmicos.

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,20 +1,32 @@
+import { stripe } from '@/lib/stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
-import { useRouter } from "next/router";
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import Stripe from 'stripe';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
 
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageUrl} width={520} height={480} alt='' />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Aliqua adipisicing culpa velit dolor sunt pariatur occaecat esse. Cillum ipsum qui duis cillum veniam. Anim laborum sint culpa nulla do exercitation occaecat culpa consectetur nostrud culpa est et. Lorem Lorem aute cillum eu amet ut velit ullamco ea consequat esse enim. Voluptate occaecat exercitation aliqua mollit eu ea sint. Duis ipsum ipsum eu ad eiusmod adipisicing. Ipsum ut irure amet ex adipisicing aliquip enim.</p>
+        <p>{product.description}</p>
 
         <button>
           Comprar agora
@@ -23,3 +35,29 @@ export default function Product() {
     </ProductContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps<any, { id: string; }> = async ({ params }) => {
+  const productId = params!.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
