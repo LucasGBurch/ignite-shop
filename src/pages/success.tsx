a810ec436d6a56a7ef39e314ea95cef05f4b601b
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-20 09:29:11 -0300

    Estrutura e estilos da página Success
    Criação do componente e seus componentes estilizados.

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,23 @@
+import { ImageContainer } from '@/styles/pages/success';
+import { SuccessContainer } from '@/styles/pages/success';
+import Link from 'next/link';
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Nome</strong>, sua camiseta <strong>NomeCamiseta</strong> já está a caminho da sua casa
+      </p>
+
+      <Link href='/'>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   );
 }
